cdea97062d298fdaf1533b5524462397c30016f6
Martin-ITT <martinusko@gmail.com>
2021-05-25 17:17:38 +0000

    second commit

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -11,7 +11,7 @@ connection = pymysql.connect(host='localhost',
                             password='',
                             db='Chinook')
 
-
+"""
 try:
     #Run a query
     with connection.cursor() as cursor:
@@ -20,10 +20,44 @@ try:
         result = cursor.fetchall()
         print(result)
 
+finally:
+    #Close the connection, regardless of whether the above was successful
+    connection.close()
+"""
+#git rm -r --cached Chinook_MySql_AutoIncrementPKs.sql
+#echo '*.sql' >> .gitignore
+
+"""
+cursor returning tuple
+try:
+    #Run a query
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM Artist;"
+        cursor.execute(sql)
+        for row in cursor:
+            print(row)
 
 finally:
     #Close the connection, regardless of whether the above was successful
     connection.close()
 
-#git rm -r --cached Chinook_MySql_AutoIncrementPKs.sql
-#echo '*.sql' >> .gitignore
+"""
+
+"""
+cursor returnig dictionary
+"""
+try:
+    #Run a query
+    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+        sql = "SELECT * FROM Genre;"
+        cursor.execute(sql)
+        for row in cursor:
+            print(row)
+
+finally:
+    #Close the connection, regardless of whether the above was successful
+    connection.close()
+
+"""
+create a table
+"""
